78980e52cefb9c3fd294a1039a93228c62c6972c
XenaN <nikitina.xena@gmail.com>
2022-10-04 15:52:48 +0700

    added yolov5

--- scripts/cli.py
+++ scripts/cli.py
@@ -4,6 +4,7 @@ from api import create_annotations
 from api import create_config
 from api import create_list_files
 from api import create_test_dataset
+from api import create_config_yolov5
 
 
 @click.group()
@@ -23,7 +24,7 @@ def ann(inp, output):
 @click.option("--input", "inp", type=click.Path(exists=True))
 @click.option("--output", type=click.Path())
 def conf(inp, output):
-    """Create yolo config"""
+    """Create yolov7 config"""
     create_config(inp, output)
 
 
@@ -41,10 +42,19 @@ def dataset(inp):
     create_test_dataset(inp)
 
 
+@click.command()
+@click.option("--input", "inp", type=click.Path(exists=True))
+@click.option("--output", type=click.Path())
+def conf5(inp, output):
+    """Create yolov5 config"""
+    create_config_yolov5(inp, output)
+
+
 cli.add_command(ann)
 cli.add_command(conf)
 cli.add_command(lists)
 cli.add_command(dataset)
+cli.add_command(conf5)
 
 
 if __name__ == "__main__":
